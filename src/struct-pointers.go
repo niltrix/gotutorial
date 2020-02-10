3fc859fb1117a66ca26e506eed39b14877cdab83
niltrix <axlgilby@gmail.com>
2020-02-10 05:42:45 -0800

    add example & gitignore

---
+++ src/struct-pointers.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	
+	v := Vertex{1, 2}
+	p := &v
+	r := &p
+	s := &r
+	fmt.Println(reflect.TypeOf(p).String())
+	fmt.Println(&p)
+	fmt.Println(reflect.TypeOf(r).String())
+	fmt.Println(&r)
+	fmt.Println(reflect.TypeOf(s).String())
+	fmt.Println(&s)
+	p.X = 1
+	fmt.Println(v)
+	(*r).X = 2
+	fmt.Println(v)
+	(*(*s)).X = 3
+	fmt.Println(v)
+}
