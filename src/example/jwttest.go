8d1148993c8e2429bb0c03eeb8be87bd091f9ace
niltrix <axlgilby@gmail.com>
2020-02-12 12:33:19 -0800

    add jwtsample

---
+++ src/example/jwttest.go
@@ -0,0 +1,19 @@
+package example
+
+import (
+	"fmt"
+
+	"github.com/brianvoe/sjwt"
+)
+
+func JwtTest() {
+	// Set Claims
+	claims := sjwt.New()
+	claims.Set("username", "billymister")
+	claims.Set("account_id", 8675309)
+
+	// Generate jwt
+	secretKey := []byte("secret_key_here")
+	jwt := claims.Generate(secretKey)
+	fmt.Println(jwt)
+}
