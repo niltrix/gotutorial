4ac650b858d7cf6a50e05a98f381dffff800c47c
niltrix <axlgilby@gmail.com>
2022-09-05 10:42:10 +0900

    updated..

--- errGroup.go
+++ errGroup.go
@@ -9,37 +9,38 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-func test1(ctx context.Context) error {
+func counterWork5(ctx context.Context) error {
 	timer := time.NewTimer(time.Second * 5)
 	select {
 	case <-timer.C:
-		fmt.Println("test1 timer")
+		fmt.Println("5sec timer")
 		return nil
 	case <-ctx.Done():
-		fmt.Println("test1 cancel")
+		fmt.Println("ctx.Done in 5sec timer")
 		return ctx.Err()
 	}
 }
 
-func test2(ctx context.Context) error {
+func counterWork2(ctx context.Context) error {
 	timer := time.NewTimer(time.Second * 2)
 	select {
 	case <-timer.C:
-		fmt.Println("test2 timer")
-		fmt.Println("Occur error test2")
-		return errors.New("error test2")
+		fmt.Println("2sec timer")
+		fmt.Println("Occur error 2sec timer")
+		return errors.New("error 2sec timer")
 	case <-ctx.Done():
-		fmt.Println("test2 cancel")
+		fmt.Println("ctx.Done in 2sec timer ")
 		return ctx.Err()
 	}
 }
 
 func main() {
+
 	ctx, _ := context.WithCancel(context.Background())
 	errGrp, errCtx := errgroup.WithContext(ctx)
 
-	errGrp.Go(func() error { return test1(errCtx) })
-	errGrp.Go(func() error { return test2(errCtx) })
+	errGrp.Go(func() error { return counterWork5(errCtx) })
+	errGrp.Go(func() error { return counterWork2(errCtx) })
 
 	//cancel()
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gotutorial
 
 go 1.17
+
+require golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde
